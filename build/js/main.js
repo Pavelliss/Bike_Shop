b646d21f994f52a8d30f644f9ea67ed9f7ec7a82
Pavel <pav-kovrizhko@mail.ru>
2020-02-10 16:58:45 +0300

    main-page

--- build/js/main.js
+++ build/js/main.js
@@ -1,21 +1,50 @@
 'use strict';
-var swiper = new Swiper('.swiper-container', {
-  slidesPerView: 3,
-  direction: getDirection(),
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  on: {
-    resize: function () {
-      swiper.changeDirection(getDirection());
+// SWEPER
+(function () {
+  var swiperBikes = new Swiper('#slider-bikes', {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    loop: true,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 47,
+        slidesPerGroup: 3,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 47,
+        slidesPerGroup: 2,
+      }
     }
-  }
-});
+  });
 
-function getDirection() {
-  var windowWidth = window.innerWidth;
-  var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+  var swiperAccessories = new Swiper('#slider-accessories', {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    loop: true,
 
-  return direction;
-}
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+      1200: {
+        slidesPerView: 4,
+        slidesPerGroup: 1,
+        spaceBetween: 104,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 39,
+        slidesPerGroup: 1,
+      }
+    }
+  });
+}());
